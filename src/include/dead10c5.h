cf86664cd73594b194afdfe1f387d050cdf0e638
franklin <2730246+devsecfranklin@users.noreply.github.com>
2023-04-04 20:26:24 -0400

    testing mpu6050

--- src/include/dead10c5.h
+++ src/include/dead10c5.h
@@ -16,22 +16,20 @@
 
 #define VERSION "v0.2 - 03 April 2023 - гопник badge - Путин хуйло"
 
-#include <Arduino.h>        // default library for Arduino
-#include <Wire.h>           // used for the I2C communication
+#include <Arduino.h> // default library for Arduino
+// #include <Wire.h>           // used for the I2C communication
 #include <SoftwareSerial.h> // used for serial output on the badge
+// #include <TinyI2CMaster.h> // this one says it is not compatible with 841
+#include "TinyWireM.h"
 
 const uint8_t BOTTOM_ROW = PA0; // PB0 on pin2
 const uint8_t MIDDLE_ROW = PA1; // PB1 on pin3
 const uint8_t TOP_ROW = PB2;    // PB2 on pin5
 
-// MPU6050
-const int MPU = 0x68; // MPU6050 I2C address
-float AccX, AccY, AccZ;
-float GyroX, GyroY, GyroZ;
-float accAngleX, accAngleY, gyroAngleX, gyroAngleY, gyroAngleZ;
-float roll, pitch, yaw;
-float AccErrorX, AccErrorY, GyroErrorX, GyroErrorY, GyroErrorZ;
-float elapsedTime, currentTime, previousTime;
-int c = 0;
+// MPU6050 https://github.com/nadavmatalon/TinyWireM/tree/master/examples/TinyWireM_example/ATtiny841_Master
+const byte SLAVE_ADDR = 100;
+unsigned long timeNow;
+boolean slavePresent = false;
+byte data = 0;
 
 #endif
