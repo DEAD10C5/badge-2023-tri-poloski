0ab230f004bc0f4030181c3d468d24e6f4c6404f
Franklin Diaz <fdiaz@paloaltonetworks.com>
2023-05-31 20:48:02 -0400

    fix script to update reference designators on puzzle footprints

---
+++ pcb/fix.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# thedevilsvoice@dead10c5.org
+# wrote this so I wouldn't have to click all the puzzle footprints
+
+# this will preserve the leading whitespace when line gets echoed
+IFS=''
+
+# reset the counter each time just to keep the numbers from getting ridiculously large
+num=0
+while read line; do num=$((++num)) && echo $line|sed "s/fp_text reference \"olive/&"$num"/"; done < adidas.kicad_pcb  > test/test.out
+num=0
+while read line; do num=$((++num)) && echo $line|sed "s/fp_text reference \"melon1/&"$num"/"; done < test/test.out > test/test2.out
+num=0
+while read line; do num=$((++num)) && echo $line|sed "s/fp_text reference \"APPLE2/&"$num"/"; done < test/test2.out > test/test3.out
+num=0
+while read line; do num=$((++num)) && echo $line|sed "s/fp_text reference \"comm/&"$num"/"; done < test/test3.out > test/test.kicad_pcb
+
